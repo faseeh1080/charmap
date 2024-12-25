78c28ae2094255af4bf8183a91742ea8901c1171
Faseeh Zaman <faseeh1080@outlook.com>
2024-12-25 10:18:13 +0530

    Make the code more maintainable.

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-root.title("Faseeh's CharMap")
-root.attributes('-topmost', True)
-
-# Frame to place character buttons.
-charFrame = ttk.Frame(root, padding="6 6 6 6")
-charFrame.grid(column=0, row=0, sticky=(N, W, E, S))
-
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
 def pasteToClipboard(stringToPaste):
     root.clipboard_clear()
     root.clipboard_append(stringToPaste)
 
-chars = "∑∏∞∂∫∇±≠≤≥≈∝√∛∜∴∵∟⊥∞"
-charButtons = []
-numberOfColumns = 5
-currentRow = 0
-currentColumn = 0
-for char in chars:
-    charButtons.append(
-        ttk.Button(
-            charFrame,
-            text=char,
-            width=5,
-            command=lambda c=char: pasteToClipboard(c)
+def createCharButtons(frame, chars):
+    charButtons = []
+    for char in chars:
+        charButtons.append(
+            ttk.Button(
+                frame,
+                text=char,
+                width=5,
+                command=lambda c=char: pasteToClipboard(c)
+            )
         )
-    )
-    if currentColumn >= numberOfColumns:
-        currentRow += 1
-        currentColumn = 0
-    charButtons[-1].grid(row=currentRow, column=currentColumn)
-    currentColumn += 1
+    return charButtons
+
+def gridButtons(buttons, noOfColumns):
+    currentRow = 0
+    currentColumn = 0
+    for button in charButtons:
+        if currentColumn >= noOfColumns:
+            currentRow += 1
+            currentColumn = 0
+        button.grid(row=currentRow, column=currentColumn)
+        currentColumn += 1
+
+# Logic
+chars = "∑∏∞∂∫∇±≠≤≥≈∝√∛∜∴∵∟⊥∞"
+
+# Window
+root = Tk()
+root.title("Faseeh's CharMap")
+root.attributes('-topmost', True)
+
+# Containers
+charFrame = ttk.Frame(root, padding="6 6 6 6") # To place character buttons.
+charFrame.grid(column=0, row=0, sticky=(N, W, E, S))
+
+# Widgets
+charButtons = createCharButtons(charFrame, chars)
+gridButtons(charButtons, 5)
 
 root.mainloop()
