7d6e4914da5778f45845b7cbac52dfb52e6916af
Faseeh Zaman <faseeh1080@outlook.com>
2024-12-25 14:06:04 +0530

    Remove tearoff from menu.

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def alwaysOnTopCheckButtonAction():
 root = Tk()
 root.title("Faseeh's CharMap")
 root.attributes('-topmost', True)
+root.resizable(False, False)
 
 # Logic
 chars = "∑∏∞∂∫∇±≠≤≥≈∝√∛∜∴∵∟⊥∞"
@@ -50,21 +51,19 @@ rootMenu = Menu(root)
 root.config(menu=rootMenu)
 
 ## File Menu
-fileMenu = Menu(rootMenu)
+fileMenu = Menu(rootMenu, tearoff=0)
 fileMenu.add_command(label="Open Favourites File", command=lambda: print("open favourites"))
 fileMenu.add_command(label="Clear Favourites")
 fileMenu.add_separator()
 fileMenu.add_command(label="Exit", command=lambda: root.destroy())
 
 ## View Menu
-viewMenu = Menu(rootMenu)
+viewMenu = Menu(rootMenu, tearoff=0)
 viewMenu.add_checkbutton(label="Always on Top", variable=alwaysOnTop, command=alwaysOnTopCheckButtonAction)
 viewMenu.add_command(label="Scale 1.0", command=pasteToClipboard("Scale"))
 viewMenu.add_command(label="Scale 1.2", command=pasteToClipboard("Scale"))
 viewMenu.add_command(label="Scale 1.4", command=pasteToClipboard("Scale"))
 viewMenu.add_command(label="Scale 1.6", command=pasteToClipboard("Scale"))
-viewMenu.add_command(label="Scale 1.8", command=pasteToClipboard("Scale"))
-viewMenu.add_command(label="Scale 2.0", command=pasteToClipboard("Scalea"))
 viewMenu.add_separator()
 viewMenu.add_command(label="Reset Scaling", command=pasteToClipboard("Reset"))
 
