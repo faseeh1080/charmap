d552800a76b778cd3310870adcb55281ac3feadb
Faseeh Zaman <faseeh1080@outlook.com>
2024-12-27 12:16:01 +0530

    Update 'package.bat' to copy 'assets' folder.

--- package.bat
+++ package.bat
@@ -1,2 +1,18 @@
-pyinstaller --icon=assets\icon.ico --name="Faseehs CharMap" main.py
+pyinstaller --icon=assets\icon.ico --name="Faseehs CharMap" --noconsole main.py
+
+set source="assets"
+set destination="dist\Faseehs CharMap\assets"
+
+:: Check if the destination folder exists
+if not exist %destination% (
+    echo The destination folder does not exist. Creating it now...
+    mkdir %destination%
+)
+
+:: Update the copy of the folder
+echo Updating the copy...
+xcopy /E /H /Y /D %source% %destination%
+
+echo Success!
+
 pause
